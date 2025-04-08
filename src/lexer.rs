ebed669cdb6f929a7933db5ae2fdf89c1ea97c79
BigWalrus <morgjik@gmail.com>
2025-04-08 23:48:40 +0200

    added empty characters

--- src/lexer.rs
+++ src/lexer.rs
@@ -7,9 +7,18 @@ pub struct Token {
 #[derive(Debug)]
 pub enum TokenType {
     Obj,
-    Endobj,
-    EmptySpace,
+    EndObj,
     Raw,
+    Null,
+    Space,
+    Tab,
+    LF,
+    FF,
+    CR,
+    Greater,
+    Smaller,
+    Slash,
+    Backslash,
 }
 
 pub fn tokenize(raw_data: Vec<u8>) {
@@ -19,34 +28,74 @@ pub fn tokenize(raw_data: Vec<u8>) {
         let byte = raw_data[i];
         match byte {
             0x6f => {
-                print!("o {:?}", TokenType::Obj);
-            }
-
-            0x62 => {
-                print!("b {:?}", TokenType::Obj);
-            }
-
-            0x6a => {
-                print!("j {:?}", TokenType::Obj);
+                if raw_data[i + 1] == 0x62 && raw_data[i + 2] == 0x6a {
+                    print!("{:?}", TokenType::Obj);
+                    i += 2;
+                } else {
+                    print!("{:?}", TokenType::Raw);
+                }
             }
 
             0x65 => {
-                print!("e {:?}", TokenType::Endobj);
+                if raw_data[i + 1] == 0x6e
+                    && raw_data[i + 2] == 0x64
+                    && raw_data[i + 3] == 0x6f
+                    && raw_data[i + 4] == 0x62
+                    && raw_data[i + 5] == 0x6a
+                {
+                    print!("{:?}", TokenType::EndObj);
+                    i += 5;
+                } else {
+                    print!("{:?}", TokenType::Raw);
+                }
             }
 
-            0x6e => {
-                print!("n {:?}", TokenType::Endobj);
+            0x3c => {
+                print!("{:?}", TokenType::Smaller);
             }
 
-            0x64 => {
-                print!("d {:?}", TokenType::Endobj);
+            0x3e => {
+                print!("{:?}", TokenType::Greater);
             }
+
+            0x2f => {
+                print!("{:?}", TokenType::Slash);
+            }
+
+            0x5c => {
+                print!("{:?}", TokenType::Backslash);
+            }
+
+            0x00 => {
+                print!("{:?}", TokenType::Null);
+            }
+
+            0x09 => {
+                print!("{:?}", TokenType::Tab);
+            }
+
+            0x0A => {
+                print!("{:?}", TokenType::LF);
+            }
+
+            0x0C => {
+                print!("{:?}", TokenType::FF);
+            }
+
+            0x0D => {
+                print!("{:?}", TokenType::CR);
+            }
+
+            0x20 => {
+                print!("{:?}", TokenType::Space);
+            }
+
             _ => {
                 print!("{:?}", TokenType::Raw);
             }
         }
 
         print!(" ");
-        i = i + 1;
+        i += 1;
     }
 }
